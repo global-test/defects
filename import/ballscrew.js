0f4e1981ad8c8d387b8e0dd3f3cef23a5468ef90
dmitriy a. sivachev <saddam0977@gmail.com>
2021-02-18 16:43:33 +0300

    добавлен импорт в скрипты;

---
+++ import/ballscrew.js
@@ -0,0 +1,8 @@
+var d_inner = get_parameter("internalD");
+var d_outer = get_parameter("outerD");
+var d_roller = get_parameter("rollerD");
+var angle = get_parameter("angle");
+var d_cage = (d_inner + d_outer) / 2;
+var z = get_parameter("rollcount");
+
+var f_nut = 0.5 * freq * (1.0 - d_roller * math.cos(angle) / d_cage) * z;
